66a2d90022f840d4460cc92cd69dd118b0160cae
Arthur Borba Lins <arthurborbalins@gmail.com>
2025-06-19 21:36:48 -0300

    feat: implementa páginas e componentes da avaliação
    - Cria páginas: Autoavaliacao, Avaliacao360, Mentoring e Referencias
    - Adiciona componentes de formulário para cada página
    - Adiciona Header, Topbar, ConfirmModal e ErrorModal
    - Implementa validações reutilizáveis em utils
    - Separa mensagens de erro em utils
    - Configura envio via fetch na avaliação (ainda sem API)

---
+++ src/pages/Avaliacao360.tsx
@@ -0,0 +1,9 @@
+import Avaliacao360Form from "../components/Avaliacao360Form";
+
+export default function Avaliacao360() {
+  return (
+    <main className="p-6">
+      <Avaliacao360Form />
+    </main>
+  );
+}
